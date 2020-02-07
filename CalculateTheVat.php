57c4af7bced70c520a7299c18171b0ee407a2139
AHFahad <53812182+AHFahad@users.noreply.github.com>
2020-02-07 19:28:46 +0600

    calculate vat done

--- CalculateTheVat.php
+++ CalculateTheVat.php
@@ -4,6 +4,17 @@
 	<title>Calculate The Vate</title>
 </head>
 <body>
+	<form method="post">
+		Input the amount :
+		<input type="number" name="amount"><br>
+		<input type="submit" value="Calculate" name="calculate">
+	</form>
 
+	<?php 
+		if(isset($_POST["calculate"])){
+			echo "The amount of vat is :".$_POST["amount"]*0.15;
+		}
+
+	 ?>
 </body>
 </html>
